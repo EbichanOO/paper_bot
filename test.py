ea892cf77509481a6fc87c40bedc7d8a10a91ce5
EbichanOO <takanori.sao@icloud.com>
2021-05-18 20:39:26 +0900

    [fix]action file

--- test.py
+++ test.py
@@ -2,13 +2,16 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def index():
-    result = calc(1,2)
-    return "<p>Hello, World!"+ str(result) +"</p>"
+    result = calc(1, 2)
+    return "<p>Hello, World!" + str(result) + "</p>"
 
-def calc(x:int,y:int) -> int:
+
+def calc(x: int, y: int) -> int:
     return x+y
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run()
